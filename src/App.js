2ae441c6ad65df358fab2b15036ebc7c11ca480a
kirikirimai <go.revolution.no3@gmail.com>
2023-02-17 14:45:24 +0900

    add modal

--- src/App.js
+++ src/App.js
@@ -1,71 +1,40 @@
 import "./App.css";
-import React, { useState } from "react";
-import { Button, Modal } from "@mui/material";
+import React, { useState,useEffect } from "react";
 
-import styled from "@emotion/styled";
-import { Box } from "@mui/system";
+import ModalBlock from "./components/ModalBlock"
 
 function App() {
-  const [open, setOpen] = useState(false);
+  const [isOpen, setOpen] = useState(false);
 
-  const handleOpen = () => {
-    setOpen(true);
-  };
+  useEffect(() => {
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+    const getParam= ()=>{
+      var url = new URL(window.location.href);
+        // URLSearchParamsオブジェクトを取得
+        let params = url.searchParams;
+        let p=false;
+
+        if(params.has('id') ) {
+          p=JSON.parse(params.get("id").toLowerCase());
+          setOpen(p)
+        }else{
+          setOpen(false)
+        }
+      }
+    getParam()
+  },[])
+  
 
   return (
     <div className="App">
       <h1>MUIテスト</h1>
-
-      <hr />
-      <h2>モーダルを試してみる</h2>
-
-      <Button onClick={handleOpen}>Open modal</Button>
-      <Modal open={open} onClose={handleClose}>
-        {/* <Box sx={{ ...style, width: 400 }}> */}
-        <OriginalModalStyle>
-          <h2>Text in a modal</h2>
-          <p>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </p>
-          <p>
-            <a href="https://www.yahoo.co.jp">yahoo japan</a>
-          </p>
-        </OriginalModalStyle>
-      </Modal>
+        <ModalBlock flg={isOpen} />
+      <hr/>
+      <h2>コンポーネントから呼び出す</h2>
+      {isOpen && <ModalBlock flg={isOpen} /> }
     </div>
   );
 }
 
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  pt: 2,
-  px: 4,
-  pb: 3,
-};
 
-const OriginalModalStyle = styled(Box)`
-  position:absolute;
-  top:50%;
-  left:50%;
-  transform: translate(-50%, -50%);
-  width:400px;
-  background-color:#ffff00;
-  border:2px solid #000;
-  
-  p{
-    font-size: 40px;
-    font-weight: bold;
-  }
-`;
 export default App;
